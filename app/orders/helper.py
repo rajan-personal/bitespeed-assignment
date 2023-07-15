bc05b60a3a07129a240402c89ed990efa8bed120
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2023-07-16 03:33:40 +0530

    check if primary orders exist and merge

--- app/orders/helper.py
+++ app/orders/helper.py
@@ -24,17 +24,33 @@ def pre_serialize(serializer):
 def presave_order(serializer):
     email = serializer.validated_data.get('email')
     phoneNumber = serializer.validated_data.get('phoneNumber')
-    primary_order = validate_user(email, phoneNumber)
+    primary_order = check_if_primary_order_exists(email, phoneNumber)
     if primary_order:
         serializer.validated_data['linkPrecedence'] = LinkPrecedence.SECONDARY
         serializer.validated_data['linkedId'] = primary_order.id
     return serializer
+    
+def check_if_primary_order_exists(email, phoneNumber):
+    primary_order_email = Order.objects.filter(linkPrecedence=LinkPrecedence.PRIMARY).filter(Q(email=email) & Q(email__isnull=False)).exclude(deletedAt__isnull=False).order_by('createdAt').first()
+    primary_order_phoneNumber = Order.objects.filter(linkPrecedence=LinkPrecedence.PRIMARY).filter(Q(phoneNumber=phoneNumber) & Q(phoneNumber__isnull=False)).exclude(deletedAt__isnull=False).order_by('createdAt').first()
+    if primary_order_email and primary_order_phoneNumber:
+        if primary_order_email.id != primary_order_phoneNumber.id:
+            primary_order = primary_order_email
+            secondary_order = primary_order_phoneNumber
+            if primary_order_phoneNumber.createdAt < primary_order_email.createdAt:
+                primary_order = primary_order_phoneNumber
+                secondary_order = primary_order_email
 
-def validate_user(email, phoneNumber):
-    primary_order = Order.objects.filter(linkPrecedence=LinkPrecedence.PRIMARY).filter((Q(email=email) & (Q(email__isnull=False)) | Q(phoneNumber=phoneNumber) & Q(phoneNumber__isnull=False))).exclude(deletedAt__isnull=False).order_by('createdAt').first()
-    if primary_order:
-        return primary_order
-
+            secondary_order.linkPrecedence = LinkPrecedence.SECONDARY
+            secondary_order.linkedId = primary_order.id
+            secondary_order.save()
+            Order.objects.filter(linkedId=secondary_order.id).update(linkedId=primary_order.id)
+            return primary_order
+    elif primary_order_email:
+        return primary_order_email
+    elif primary_order_phoneNumber:
+        return primary_order_phoneNumber
+    
 
 
     
